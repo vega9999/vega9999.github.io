efec2f8d912f9b1f63d92e5f011ba8210d98223d
vega9999 <70079653+vega9999@users.noreply.github.com>
2025-02-27 03:29:34 +0100

    Scroll top btn

--- assets/js/content.js
+++ assets/js/content.js
@@ -486,3 +486,24 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// Scroll top
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollToTopBtn = document.getElementById("scrollToTop");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+            scrollToTopBtn.classList.add("show");
+        } else {
+            scrollToTopBtn.classList.remove("show");
+        }
+    });
+
+    scrollToTopBtn.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
+});
+
+
